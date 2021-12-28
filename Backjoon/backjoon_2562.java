b19177016e12a85159d188ec8c69bdc58e89d3a2
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2021-12-29 03:46:32 +0900

    Update backjoon_2562.java

--- Backjoon/backjoon_2562.java
+++ Backjoon/backjoon_2562.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class backjoon_2562 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
 
-        int[] arr = {3, 29, 38, 12, 57, 74, 40, 85, 61};
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
 
         int max = arr[0];
         int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>max){
+            if (arr[i] > max) {
                 max = arr[i];
-                count = i+1;
+                count = i + 1;
             }
         }
         System.out.println(max);
