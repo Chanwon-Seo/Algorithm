713a2dcbdc2defebc763aff81a0356cffe9daf4c
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2021-12-29 16:52:12 +0900

    Create backjoon_15596.java

---
+++ Backjoon/backjoon_15596.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+class Test {
+    public static long sum(int[] a) {
+        long sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
+
+public class backjoon_15596 {
+    public static void main(String[] args) {
+    }
+}
