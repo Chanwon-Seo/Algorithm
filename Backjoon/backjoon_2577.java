a8c7db09a0e8b9d7dd9094b72b4ed23d6b44c945
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2022-02-04 15:25:08 +0900

    Update backjoon_2577.java

--- Backjoon/backjoon_2577.java
+++ Backjoon/backjoon_2577.java
@@ -6,13 +6,13 @@ public class backjoon_2577 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int sum = (sc.nextInt() * sc.nextInt() * sc.nextInt());
-        String str = Integer.toString(sum);
+        int sum = sc.nextInt() * sc.nextInt() * sc.nextInt();
+        String strVal = Integer.toString(sum);
 
         for (int i = 0; i < 10; i++) {
             int count = 0;
-            for (int j = 0; j < str.length(); j++) {
-                if ((str.charAt(j) - '0') == i) {
+            for (int j = 0; j < strVal.length(); j++) {
+                if ((strVal.charAt(j) - '0') == i) {
                     count++;
                 }
             }
