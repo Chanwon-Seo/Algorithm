1e687b3d9a75d145053c192bd3acfdb412687d71
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2022-02-04 15:25:15 +0900

    Create backjoon_10926.java

---
+++ Backjoon/backjoon_10926.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class backjoon_10926 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+
+        System.out.println(id + "??!");
+    }
+}
