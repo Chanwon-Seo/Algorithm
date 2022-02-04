c90ce763077319e3b4c7fddf0060fa53091002af
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2022-02-04 15:25:11 +0900

    Update backjoon_4673.java

--- Backjoon/backjoon_4673.java
+++ Backjoon/backjoon_4673.java
@@ -1,32 +1,31 @@
 package backjoon;
 
 public class backjoon_4673 {
-    public static void main(String[] args) {
-        int array[] = new int[10001];
-
-
-        for(int i=1; i<= array.length; i++) {
-            int result = calculate(i);
-            if(result <= 10000) {
-                array[result] = 1;
-            }
-        }
-
-        for(int i=1; i< array.length; i++) {
-            if(array[i] != 1) {
-                System.out.println(i);
-            }
-        }
-    }
-
     public static int calculate(int value) {
         int result = value;
 
-        while(value > 0) {
+        while (value > 0) {
             result += value % 10;
             value /= 10;
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        int array[] = new int[10001];
+
+        for (int i = 1; i <= array.length; i++) {
+            int result = calculate(i);
+            if (result <= 10000) {
+                array[result] = 1;
+            }
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != 1) {
+                System.out.println(i);
+            }
+        }
     }
 }
