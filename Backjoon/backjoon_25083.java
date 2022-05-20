819f3d288587e0139e36e61c6ee263477e8b4b6d
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2022-05-20 14:59:43 +0900

    backjoon_25083

---
+++ Backjoon/backjoon_25083.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+public class backjoon_25083 {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.print(s);
+    }
+}
