4b52eb6f37cb6d7df2e3645c4bd628dab2a49737
Chanwon-Seo <scwonn60@gmail.com>
2022-09-13 14:52:44 +0900

    평균

---
+++ backjoon/backjoon_3052.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[42];
+
+        for (int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+
+        int count = 0;
+        for (boolean result : arr) {
+            if (result) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
