0c77efa1fbd793f3c7302137a58fcb2cf76c2b9c
Chanwon-Seo <scwonn60@gmail.com>
2022-09-28 01:28:50 +0900

    검증수

---
+++ backjoon/구현/backjoon_2475.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i] * arr[i];
+        }
+
+        System.out.println(result % 10);
+    }
+}
