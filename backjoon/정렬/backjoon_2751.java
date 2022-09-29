3b84b2064126d6c0a41dfa65712436c1892314a7
Chanwon-Seo <scwonn60@gmail.com>
2022-09-30 01:08:56 +0900

    수 정렬하기 2

---
+++ backjoon/정렬/backjoon_2751.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
