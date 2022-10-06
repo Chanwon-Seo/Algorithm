9caeb00a17e8349da931d1728c0e2ee08f98c6d7
Chanwon-Seo <scwonn60@gmail.com>
2022-10-06 22:30:40 +0900

    !밀비 급일

---
+++ backjoon/문자열/backjoon_11365.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb;
+
+        while (true) {
+            String str = br.readLine();
+            if (str.equals("END")) {
+                break;
+            }
+            sb = new StringBuilder().append(str).reverse();
+            System.out.println(sb);
+        }
+    }
+}
