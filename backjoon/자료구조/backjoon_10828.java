b4d591ac12749866a7c1a0e083a93a82614999f2
Chanwon-Seo <scwonn60@gmail.com>
2022-10-07 22:30:53 +0900

    스택

---
+++ backjoon/자료구조/backjoon_10828.java
@@ -0,0 +1,48 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push")) {
+                stack.push(Integer.valueOf(st.nextToken()));
+            } else if (str.equals("pop")) {
+                if (!stack.empty()) {
+                    sb.append(stack.pop() + "\n");
+                } else {
+                    sb.append("-1\n");
+                }
+            } else if (str.equals("size")) {
+                sb.append(stack.size() + "\n");
+            } else if (str.equals("empty")) {
+                if (!stack.empty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append("1\n");
+                }
+            } else {
+                if (!stack.empty()) {
+                    sb.append(stack.peek() + "\n");
+                } else {
+                    sb.append("-1\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
