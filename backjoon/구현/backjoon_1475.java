23a897978d2aa93844cae7f5e3df17e1d4acc754
Chanwon-Seo <scwonn60@gmail.com>
2023-04-05 13:57:25 +0900

    방 번호

---
+++ backjoon/구현/backjoon_1475.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[10];
+
+        for (int i = 0; i < str.length(); i++) {
+            int num = str.charAt(i) - '0';
+            if (num == 9) {
+                num = 6;
+            }
+            arr[num]++;
+        }
+
+        arr[6] = arr[6] / 2 + arr[6] % 2;
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[9]);
+    }
+}
