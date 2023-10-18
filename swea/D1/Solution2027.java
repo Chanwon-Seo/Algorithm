6a5b1d31d10e99aa3acc5426d512564e2fa98e2a
Chanwon-Seo <scwonn60@gmail.com>
2023-10-18 23:17:48 +0900

    2027. 대각선 출력하기

---
+++ swea/D1/Solution2027.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
+                if (i == j) {
+                    System.out.print("#");
+                    continue;
+                }
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+    }
+}
