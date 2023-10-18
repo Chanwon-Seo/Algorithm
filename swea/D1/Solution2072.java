1c5dfac49a279f2b1139a22e51252dee047a904b
Chanwon-Seo <scwonn60@gmail.com>
2023-10-18 15:12:41 +0900

    Update 2072. 홀수만 더하기

--- swea/D1/Solution2072.java
+++ swea/D1/Solution2072.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Solution2072 {
+public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
