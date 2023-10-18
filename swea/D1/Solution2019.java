ee369f13f371b6dc5a2e22bffd8493d419b9cd6b
Chanwon-Seo <scwonn60@gmail.com>
2023-10-18 23:31:17 +0900

    2019. 더블더블

---
+++ swea/D1/Solution2019.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int sum = 1;
+
+        for (int i = 0; i <= N; i++) {
+            sb.append(sum).append(" ");
+            sum *= 2;
+        }
+        System.out.println(sb);
+    }
+}
