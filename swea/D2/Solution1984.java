eee597c1d4991db1a7f7913a904dfa07651e1b5d
Chanwon-Seo <scwonn60@gmail.com>
2023-10-19 14:10:32 +0900

    1984. 중간 평균값 구하기

--- swea/D2/Solution1984.java
+++ swea/D2/Solution1984.java
@@ -5,21 +5,19 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        int[] arr = null;
+
         int T = Integer.parseInt(br.readLine().trim());
 
-        StringTokenizer st = null;
-        int[] arr = null;
         for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             arr = new int[10];
-
             for (int j = 0; j < arr.length; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
             }
+
             Arrays.sort(arr);
-
             int sum = 0;
-
             for (int z = 1; z < arr.length - 1; z++) {
                 sum += arr[z];
             }
