413d4cc091a431cfb9851749523c660d628c65da
Chanwon-Seo <scwonn60@gmail.com>
2023-10-23 20:09:07 +0900

    1859. 백만 장자 프로젝트

---
+++ swea/D2/Solution1859.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        int[] arr = null;
+
+        for (int Ti = 0; Ti < T; Ti++) {
+            int N = Integer.parseInt(br.readLine());
+            arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                arr[i] = num;
+            }
+            long max = 0;
+            long sum = 0;
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (max < arr[i]) {
+                    max = arr[i];
+                } else {
+                    sum += max - arr[i];
+                }
+            }
+            sb.append(String.format("#%d %d\n", Ti + 1, sum));
+        }
+
+        System.out.println(sb);
+    }
+}
