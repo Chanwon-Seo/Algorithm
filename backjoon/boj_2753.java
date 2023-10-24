25fe64297e6a31a11c2bc1e669fa2ca27c380289
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 14:05:21 +0900

    윤년

---
+++ backjoon/boj_2753.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        if (T % 4 == 0 && (T % 100 != 0 || T % 400 == 0)) {
+            sb.append("1");
+        } else {
+            sb.append("0");
+        }
+        System.out.println(sb);
+    }
+}
