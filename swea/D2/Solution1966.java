8b87602f9d7d1745f612a4bc396ab1c55f5752fe
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 20:16:50 +0900

    1966. 숫자를 정렬하자

---
+++ swea/D2/Solution1966.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        int[] arr = null;
+
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(br.readLine());
+            arr = new int[M];
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+            sb.append(String.format("#%d ", i + 1));
+
+            for (int j = 0; j < M - 1; j++) {
+                sb.append(String.format("%d ", arr[j]));
+            }
+            sb.append(String.format("%d\n", arr[arr.length - 1]));
+        }
+        System.out.print(sb);
+    }
+}
