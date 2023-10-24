bd53d9aaf2f55256b0a2089422274e197a827347
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 13:24:34 +0900

    X보다 작은 수

---
+++ backjoon/boj_10871.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num < X) {
+                sb.append(String.format("%d ", num));
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
