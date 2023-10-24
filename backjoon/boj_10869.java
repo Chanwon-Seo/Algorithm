65263f4f841d53a4b382d5111a3e5799167cf7a9
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 13:43:24 +0900

    사칙연산

---
+++ backjoon/boj_10869.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(String.format("%d\n", A + B));
+        sb.append(String.format("%d\n", A - B));
+        sb.append(String.format("%d\n", A * B));
+        sb.append(String.format("%d\n", A / B));
+        sb.append(String.format("%d\n", A % B));
+
+        System.out.print(sb);
+    }
+}
