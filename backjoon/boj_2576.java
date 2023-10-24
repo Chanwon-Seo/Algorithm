c18b304ccc20259ac41c994b721826642d5eaa8e
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 16:09:56 +0900

    홀수

---
+++ backjoon/boj_2576.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 7; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 != 0) {
+                sum += num;
+                if (min > num) {
+                    min = num;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.printf("%d", -1);
+        } else {
+            System.out.printf("%d\n%d", sum, min);
+        }
+    }
+}
