3d0c7ad6bd7a36a0fd29747acae18075b69fadc4
Chanwon-Seo <scwonn60@gmail.com>
2023-10-24 13:27:11 +0900

    A+B

---
+++ backjoon/boj_1000.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.printf("%d", Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+    }
+}
