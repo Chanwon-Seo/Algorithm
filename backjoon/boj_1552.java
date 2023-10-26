5f47689355b99bf96f0ac2a04fc6d91bcdc5c252
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 14:43:02 +0900

    단어의 개수

---
+++ backjoon/boj_1552.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().trim().split(" ");
+
+        int ans = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[0].equals("")) {
+                continue;
+            }
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
