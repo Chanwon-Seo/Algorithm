8df7d259245408968da755a8f09fbc02a0f707fd
Chanwon-Seo <scwonn60@gmail.com>
2023-10-27 00:00:28 +0900

    알파벳 찾기

---
+++ backjoon/boj_10809.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int[] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            int num = str.charAt(i) - 97;
+
+            if (arr[num] == -1) {
+                arr[num] = i;
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf(arr[i] + " ");
+        }
+    }
+}
