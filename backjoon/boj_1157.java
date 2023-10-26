aabe564e6b8582ff98cf0bc0df0552ec344f67e1
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 15:37:20 +0900

    단어 공부

---
+++ backjoon/boj_1157.java
@@ -0,0 +1,38 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toLowerCase();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[str.charAt(i) - 97]++;
+        }
+        int max = Integer.MIN_VALUE;
+
+        int checkI = 0;
+        String ans = "?";
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                checkI = i;
+                ans = "no";
+            } else if (arr[i] == max) {
+                ans = "?";
+            }
+
+        }
+
+        if (ans.equals("?")) {
+            System.out.println("?");
+        } else {
+            System.out.println(String.valueOf((char) (checkI + 97)).toUpperCase());
+        }
+
+    }
+}
