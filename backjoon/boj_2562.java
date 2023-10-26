13c7fc6b973afb37eb8a97c92b459543d5a1f7d9
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 16:05:42 +0900

    최댓값

---
+++ backjoon/boj_2562.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int count = 0;
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                count = i;
+            }
+        }
+        System.out.printf("%d\n%d", max, count);
+    }
+}
