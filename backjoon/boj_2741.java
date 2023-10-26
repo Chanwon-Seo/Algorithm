1b76e3e3d75b23949f746705e53acffaca184715
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 17:16:55 +0900

    N 찍기

---
+++ backjoon/boj_2741.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= T; i++) {
+            sb.append(String.format("%d\n", i));
+        }
+
+        System.out.println(sb);
+
+    }
+}
