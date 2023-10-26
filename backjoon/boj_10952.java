43af37031068992ef7dc24316feee957fd5705e4
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 23:16:55 +0900

    A+B - 5

---
+++ backjoon/boj_10952.java
@@ -0,0 +1,30 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        String str;
+
+        while ((str = br.readLine()) != null) {
+
+            st = new StringTokenizer(str, " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A == 0 && B == 0) {
+                break;
+            }
+            sb.append(String.format("%d\n", A + B));
+        }
+
+        System.out.println(sb);
+
+    }
+}
