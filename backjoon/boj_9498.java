eea3a7010fce7f4a1e684327bc094e2f941a4fbc
Chanwon-Seo <scwonn60@gmail.com>
2023-10-26 23:50:18 +0900

    시험 성적

--- backjoon/boj_9498.java
+++ backjoon/boj_9498.java
@@ -1,10 +1,24 @@
+package backjoon;
+
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        System.out.println(N >= 90 ? 'A' : N >= 80 ? 'B' : N >= 70 ? 'C' : N >= 60 ? 'D' : 'F');
+
+        if (N >= 90) {
+            System.out.println("A");
+        } else if (N >= 80) {
+            System.out.println("B");
+        } else if (N >= 70) {
+            System.out.println("C");
+        } else if (N >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
     }
 }
