6cbfc85dfce311a3c657c9ec5c70d3d6f269bd47
Chanwon-Seo <scwonn60@gmail.com>
2023-10-27 10:16:34 +0900

    알람 시계

---
+++ backjoon/boj_2884.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M < 45) {
+            M = M + 60 - 45;
+            if (T == 0) {
+                T = 23;
+            } else {
+                T--;
+            }
+        } else {
+            M -= 45;
+        }
+
+        System.out.println(T + " " + M);
+    }
+}
