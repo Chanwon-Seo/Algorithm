7a00091ee66e3ddbb61c12b8d8ac77d54f28e686
Chanwon-Seo <scwonn60@gmail.com>
2023-10-27 11:45:00 +0900

    OX퀴즈

---
+++ backjoon/boj_8958.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String str = br.readLine();
+
+            int max = 0;
+            int sum = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == 'O') {
+                    sum += ++max;
+                } else {
+                    max = 0;
+                }
+            }
+            sb.append(String.format("%d\n", sum));
+        }
+        System.out.println(sb);
+    }
+}
