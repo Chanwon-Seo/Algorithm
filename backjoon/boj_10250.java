fea4b691880eea371911e16642578c910dd3b5f3
Chanwon-Seo <scwonn60@gmail.com>
2023-10-28 23:43:51 +0900

    ACM 호텔

---
+++ backjoon/boj_10250.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+
+        for (int i = 0; i < T; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            if (N % H == 0) {
+                System.out.println((H * 100) + (N / H));
+            } else {
+                System.out.println(((N % H) * 100) + (N / H) + 1);
+            }
+        }
+    }
+}
