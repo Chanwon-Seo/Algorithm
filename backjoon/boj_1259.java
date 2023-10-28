b31da356d4a753825c50cb0b8907889ef64b9a25
Chanwon-Seo <scwonn60@gmail.com>
2023-10-28 23:54:01 +0900

    팰린드롬수

---
+++ backjoon/boj_1259.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
+
+        while (true) {
+            String T = br.readLine();
+            if (T.equals("0")) {
+                break;
+            }
+            StringBuffer sb = new StringBuffer(T);
+            boolean b = T.equals(sb.reverse().toString());
+
+            if (b) {
+                stb.append("yes\n");
+            } else {
+                stb.append("no\n");
+            }
+        }
+        System.out.println(stb);
+    }
+}
