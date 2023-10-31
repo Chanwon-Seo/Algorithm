d0b37342dee00b4a94084744122e34903492f9c9
Chanwon-Seo <scwonn60@gmail.com>
2023-10-31 11:48:38 +0900

    제로

---
+++ backjoon/boj_10773.java
@@ -0,0 +1,35 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (stack.size() != 0) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (stack.size() != 0) {
+                sum += stack.pop();
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+}
