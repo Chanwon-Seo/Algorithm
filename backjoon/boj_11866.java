418cfac93d3989b7f34808e980c26e16d5443526
Chanwon-Seo <scwonn60@gmail.com>
2023-11-02 23:24:38 +0900

    요세푸스 문제 0

---
+++ backjoon/boj_11866.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Deque<Integer> dq = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for (int i = 1; i <= N; i++) {
+            dq.add(i);
+        }
+        while (N-- > 1) {
+            for (int i = 1; i < K; i++) {
+                if (!dq.isEmpty()) {
+                    dq.addLast(dq.pollFirst());
+                }
+            }
+            sb.append(dq.pollFirst()).append(", ");
+        }
+        sb.append(dq.pollFirst()).append(">");
+        System.out.println(sb);
+    }
+}
