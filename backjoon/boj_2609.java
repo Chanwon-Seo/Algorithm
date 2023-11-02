66f1b4d73fa0bfd5292a90528d7f8af771061028
Chanwon-Seo <scwonn60@gmail.com>
2023-11-02 22:51:49 +0900

    최대공약수와 최소공배수

---
+++ backjoon/boj_2609.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int num = A * B;
+        
+        while (B != 0) {
+            A = A % B;
+            int temp = A;
+            A = B;
+            B = temp;
+        }
+        System.out.println(A + "\n" + num / A);
+    }
+}
