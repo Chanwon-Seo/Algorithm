d6ab8aff25a409b207fbd7bbc53f4265f04ffbe6
Chanwon-Seo <scwonn60@gmail.com>
2023-11-03 16:45:09 +0900

    수 찾기

---
+++ backjoon/boj_1920.java
@@ -0,0 +1,55 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+
+            int start = 0;
+            int mid = 0;
+            int end = arr.length - 1;
+            int target = Integer.parseInt(st.nextToken());
+            boolean flag = false;
+            while (start <= end) {
+
+                mid = (start + end) / 2;
+                if (arr[mid] == target) {
+                    flag = true;
+                    break;
+                } else if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+                flag = false;
+            }
+
+            if (flag) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+}
