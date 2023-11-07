648eaeadde8cdab7e07cb393a0970507fc9b85f3
Chanwon-Seo <scwonn60@gmail.com>
2023-11-07 19:32:23 +0900

    랜선 자르기

---
+++ backjoon/boj_1654.java
@@ -0,0 +1,41 @@
+package backjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[K];
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long start = 1;
+
+        while (start <= max) {
+
+            long mid = (start + max) / 2;
+            long count = 0;
+
+            for (long num : arr) {
+                count += num / mid;
+            }
+            if (count < N) {
+                max = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println(max);
+    }
+}
