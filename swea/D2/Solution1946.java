eaa38412f504a03bcf8c235375402f509744585b
Chanwon-Seo <scwonn60@gmail.com>
2023-11-14 14:10:13 +0900

    1946. 간단한 압축 풀기

---
+++ swea/D2/Solution1946.java
@@ -0,0 +1,39 @@
+package swea;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int Ti = 0; Ti < T; Ti++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(String.format("#%d\n", Ti + 1));
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                String c = st.nextToken();
+                int k = Integer.parseInt(st.nextToken());
+
+                while (k-- > 0) {
+                    if (count == 10) {
+                        sb.append("\n");
+                        count = 0;
+                    }
+                    sb.append(c);
+                    ++count;
+                }
+            }
+            sb.append("\n");
+            if (Ti == T) {
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
