70e180f03a2542f31080ba7bdd84c2255c2fd488
Chanwon-Seo <scwonn60@gmail.com>
2023-11-14 19:24:09 +0900

    1284. 수도 요금 경쟁

---
+++ swea/D2/Solution1284.java
@@ -0,0 +1,37 @@
+package swea;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int Ti = 0; Ti < T; Ti++) {
+            st = new StringTokenizer(br.readLine());
+            int P = Integer.parseInt(st.nextToken());
+            int Q = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+
+            int priceA = 0;
+            int priceB = 0;
+            priceA = P * W;
+
+            if (R >= W) {
+                priceB = Q;
+            } else {
+                priceB = Q + ((W - R) * S);
+            }
+
+            sb.append(String.format("#%d %d\n", Ti + 1, Math.min(priceA, priceB)));
+        }
+
+        System.out.println(sb);
+
+    }
+}
