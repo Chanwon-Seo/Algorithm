f4f1a2dfb2045d35c593511a9f9a70f9e14f5215
Chanwon-Seo <scwonn60@gmail.com>
2023-11-15 21:40:18 +0900

    15941. 평행사변형

---
+++ swea/D3/Solution15941.java
@@ -0,0 +1,17 @@
+package swea;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int ti = 1; ti <= T; ti++) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(String.format("#%d %d\n", ti, num * num));
+        }
+        System.out.println(sb);
+    }
+}
