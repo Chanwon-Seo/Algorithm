de211eeceee9d88d036a229e7d0334930271bec6
Chanwon-Seo <scwonn60@gmail.com>
2023-11-15 22:06:05 +0900

    1289. 원재의 메모리 복구하기

---
+++ swea/D3/Solution1289.java
@@ -0,0 +1,28 @@
+package swea;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        String str = "";
+
+        for (int ti = 1; ti <= T; ti++) {
+            str = br.readLine();
+            int count = 0;
+            char ch = '0';
+            for (int i = 0; i < str.length(); i++) {
+                if (ch != str.charAt(i)) {
+                    ch = str.charAt(i);
+                    count++;
+                }
+            }
+
+            sb.append(String.format("#%d %d\n", ti, count));
+        }
+        System.out.println(sb);
+
+    }
+}
