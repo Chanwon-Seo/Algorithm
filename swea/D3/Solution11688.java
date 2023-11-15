d2d45e2fff9ffc368487618fdb32093f103942c3
Chanwon-Seo <scwonn60@gmail.com>
2023-11-15 22:20:56 +0900

    11688. Calkin-Wilf tree 1

---
+++ swea/D3/Solution11688.java
@@ -0,0 +1,30 @@
+package swea;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        String str = "";
+        for (int ti = 1; ti <= T; ti++) {
+            str = br.readLine();
+            int a = 1;
+            int b = 1;
+            int loop = str.length();
+            for (int i = 0; i < loop; i++) {
+                if (str.charAt(i) == 'L') {
+                    b = a + b;
+                } else {
+                    a = a + b;
+                }
+            }
+            sb.append(String.format("#%d %d %d\n", ti, a, b));
+        }
+        System.out.println(sb);
+
+    }
+}
