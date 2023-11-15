7397a633fa4fa29f70b1db41caaa1d68006ef98b
Chanwon-Seo <scwonn60@gmail.com>
2023-11-15 17:39:35 +0900

    14555. 공과 잡초

---
+++ swea/D3/Solution14555.java
@@ -0,0 +1,26 @@
+package swea;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int Ti = 0; Ti < T; Ti++) {
+            String str = br.readLine();
+
+            int loop = str.length() - 1;
+            int result = 0;
+            for (int i = 0, j = 2; i < loop; i++, j++) {
+                if (str.substring(i, j).equals("()") || str.substring(i, j).equals("|)")
+                        || str.substring(i, j).equals("(|")) {
+                    result++;
+                }
+            }
+            sb.append(String.format("#%d %d\n", Ti + 1, result));
+        }
+        System.out.println(sb);
+    }
+}
