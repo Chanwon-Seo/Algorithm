e57773ce39e2f07eb2575a1e057299e8517d6a25
Chanwon-Seo <scwonn60@gmail.com>
2024-06-07 09:25:44 +0900

    가격대 별 상품 개수 구하기

--- programmers/sql/가격대 별 상품 개수 구하기.sql
+++ programmers/sql/가격대 별 상품 개수 구하기.sql
@@ -1,6 +1,11 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131530
-select truncate (price,-4) price_group,
-       count(product_id) products
-from product
-group by price_group
-order by price_group
+SELECT
+    truncate (price,
+              -4) price_group,
+    count(product_id) products
+FROM
+    product
+GROUP BY
+    price_group
+ORDER BY
+    price_group
