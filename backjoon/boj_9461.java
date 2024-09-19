875d26ac87beb3b2ac72e7d4d6fc19d68e35f4a8
Chanwon-Seo <scwonn60@gmail.com>
2024-09-20 00:30:07 +0900

    파도반 수열

---
+++ backjoon/boj_9461.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_9461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i < 101; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(dp[input]).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
