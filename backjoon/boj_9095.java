96bca5eb87078916e62008a668f64c8dbd1678c2
Chanwon-Seo <scwonn60@gmail.com>
2024-09-20 00:06:28 +0900

    1, 2, 3 더하기

--- backjoon/boj_9095.java
+++ backjoon/boj_9095.java
@@ -4,29 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class boj_9095 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         ArrayList<Integer> nList = new ArrayList<>();
+
         for (int i = 0; i < T; i++) {
             int input = Integer.parseInt(br.readLine());
             nList.add(input);
         }
-        Collections.sort(nList);
 
-        Integer size = nList.get(nList.size() - 1);
-        int[] dp = new int[size + 1];
+        int[] dp = new int[11];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-        for (int i = 4; i <= size; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        for (int i = 4; i < dp.length; i++) {
+            for (int j = 1; j <= 3; j++) {
+                dp[i] += dp[i - j];
+            }
         }
+
         for (Integer i : nList) {
             System.out.println(dp[i]);
         }
