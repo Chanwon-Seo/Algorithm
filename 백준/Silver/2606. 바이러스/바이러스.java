809ed7aa69e1c533fd36908651a3c093343bd90c
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-09-21 18:29:28 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int cnt;
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+        A = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        DFS(1);
+        System.out.println(cnt);
+    }
+
+    private static void DFS(int v) {
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                cnt++;
+                DFS(i);
+            }
+        }
+    }
+
+}
