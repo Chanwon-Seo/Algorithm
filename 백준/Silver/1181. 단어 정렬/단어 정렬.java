a793928f3cbec9f1cf3e7338d9f9fda7bdac6c27
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-09-21 18:23:00 +0900

    [Silver V] Title: 단어 정렬, Time: 3572 ms, Memory: 51528 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1; j++) {
+                if (arr[j].length() > arr[j + 1].length()) {
+                    String temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i].equals(arr[i + 1])) {
+                arr[i] = "";
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i].equals("")) {
+                sb.append(String.format("%s\n", arr[i]));
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
