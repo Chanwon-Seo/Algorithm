ff43bac8c8579f3ec9887cfe86984c4daa5819e2
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-09-27 23:20:58 +0900

    [Silver II] Title: 정수 삼각형, Time: 192 ms, Memory: 24240 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,13 +4,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[][] map;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
-        int[][] dp = new int[N][N];
-
         StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < i + 1; j++) {
@@ -18,16 +20,13 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            dp[N - 1][i] = map[N - 1][i];
-        }
-
         for (int i = N - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + map[i][j];
+                int num = map[i][j];
+                map[i][j] = Math.max(map[i + 1][j], map[i + 1][j + 1]) + num;
             }
         }
 
-        System.out.println(dp[0][0]);
+        System.out.println(map[0][0]);
     }
 }
