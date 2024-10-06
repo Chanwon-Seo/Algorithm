b179c695bc0d849c47e79806670c2ddc5440c005
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-10-07 00:21:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 520 ms, Memory: 292464 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, K;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        sb.append("<");
+
+        while (q.size() > 1) {
+            for (int i = 0; i < K - 1; i++) {
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
+    }
+}
