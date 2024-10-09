30607d127483241dba94413adf76ed4cc4b30171
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-10-09 21:16:31 +0900

    [Silver IV] Title: 제곱근, Time: 112 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        BigInteger big = new BigInteger(br.readLine());
+        System.out.println(big.sqrt());
+    }
+
+}
