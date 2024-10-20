5f5ac01531a7a4b6c0ddf0e8525f50f912338717
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-10-21 00:08:25 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 144 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[] arr;
     static int N;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
         st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        int max = -1;
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int input = Integer.parseInt(st.nextToken());
+            sum += input;
+            max = Math.max(max, input);
         }
 
-        Arrays.sort(arr);
-        int sum = 0;
-        for (int i = 0; i < N - 1; i++) {
-            sum += arr[i];
-        }
-        System.out.println(sum);
+        System.out.println(sum - max);
     }
 }
