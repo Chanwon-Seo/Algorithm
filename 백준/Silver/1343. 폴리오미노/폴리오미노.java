a87262e1c0dec8f9ef81770132b21fe74fc8f04e
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-10-21 00:56:55 +0900

    [Silver V] Title: 폴리오미노, Time: 64 ms, Memory: 11660 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine()
                 .replace("XXXX", "AAAA")
                 .replace("XX", "BB");
 
-        System.out.println(input.contains("X")? -1:input);
+        System.out.println(input.contains("X") ? -1 : input);
     }
 }
