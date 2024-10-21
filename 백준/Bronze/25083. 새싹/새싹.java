a6c94d1ff67424060994194c92d3396f2ee6b9cd
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-10-21 15:38:43 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 11380 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        sb.append("         ,r'\"7").append("\n")
+                .append("r`-_   ,'  ,/").append("\n")
+                .append(" \\. \". L_r'").append("\n")
+                .append("   `~\\/").append("\n")
+                .append("      |").append("\n")
+                .append("      |").append("\n");
+
+        System.out.print(sb);
+    }
+}
