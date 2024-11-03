9eb1ee52ff0bbf55249c1c1704d155de39320aa7
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-11-03 22:17:31 +0900

    [Bronze V] Title: We love kriii, Time: 60 ms, Memory: 11244 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(sb.append("강한친구 대한육군").append("\n")
+                .append("강한친구 대한육군"));
+    }
+}
