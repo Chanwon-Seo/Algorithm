baa411f3198e69ad39015ade7f9c0d0b97e8398b
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-11-07 22:49:45 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        String input = br.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        System.out.print(sb);
+    }
+}
