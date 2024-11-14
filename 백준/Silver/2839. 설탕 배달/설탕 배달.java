2190563b17e0aca2859037b0f49c7f14eaa9d588
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-11-14 13:45:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int basket = 0;
+        while (N > 0) {
+            if (N % 5 == 0) {
+                basket += N / 5;
+                break;
+            } else {
+                N -= 3;
+                basket++;
+            }
+            if (N < 0) {
+                basket = -1;
+            }
+        }
+        System.out.println(basket);
+    }
+}
