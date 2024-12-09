6551f6e5fcb4c09ef9dfaf91e735b9940045ae9a
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-12-09 10:15:02 +0900

    [Bronze II] Title: NN, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/11944. NN/NN.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(n);
+        }
+        if (sb.length() > m) {
+            System.out.println(sb.substring(0, m));
+        } else {
+            System.out.println(sb);
+        }
+    }
+}
