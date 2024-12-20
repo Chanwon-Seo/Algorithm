a9bc337ca996ad4669d5aa449484dce17a2e5519
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2024-12-20 13:51:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 192 ms, Memory: 18548 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static long[][] dp;
+    static long mod = 1000000000;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new long[N + 1][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % mod;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % mod;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp[N]).sum() % mod);
+    }
+}
