0380b91a7104893e641bb33c66b7d817d4f77027
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-01-29 21:48:13 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        long [] answer = new long[N+1];
+
+        answer[0] = 0;
+        answer[1] = 1;
+
+        for( int i = 2; i<=N; i++){
+            answer[i] = answer[i-1]+answer[i-2];
+        }
+
+        System.out.println(answer[N]);
+    }
+}
