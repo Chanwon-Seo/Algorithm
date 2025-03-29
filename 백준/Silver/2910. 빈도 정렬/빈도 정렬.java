ee0fba85ba2204bf12278dc1e6a7c5c3087fccf0
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-03-29 11:24:19 +0900

    [Silver III] Title: 빈도 정렬, Time: 80 ms, Memory: 12120 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return map.get(o2) - map.get(o1);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : list) {
+            for (int j = 0; j < map.get(i); j++) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
