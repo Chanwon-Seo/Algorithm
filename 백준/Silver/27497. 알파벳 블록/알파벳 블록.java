520af2cae1f9af7ccdac5c31a29ee9566ebbb260
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-04-01 22:37:01 +0900

    [Silver II] Title: 알파벳 블록, Time: 704 ms, Memory: 243500 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<String> q = new LinkedList<>();
+        Stack<Boolean> stack = new Stack<>();
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String flag = st.nextToken();
+            if ("1".equals(flag)) {
+                q.offer(st.nextToken());
+                stack.push(true);
+            } else if ("2".equals(flag)) {
+                q.offerFirst(st.nextToken());
+                stack.push(false);
+            } else if (!stack.isEmpty()) {
+                if (stack.pop()) q.removeLast();
+                else q.removeFirst();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : q) {
+            sb.append(s);
+        }
+        System.out.println(sb.length() > 0 ? sb : "0");
+    }
+}
