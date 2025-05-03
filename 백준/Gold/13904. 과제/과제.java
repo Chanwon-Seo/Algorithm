87bb8db8e005b1de138cdb2da7a16a60a6599043
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-05-03 12:58:39 +0900

    [Gold III] Title: 과제, Time: 84 ms, Memory: 12184 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list.add(new int[]{d, w});
+        }
+
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        Queue<Integer> q = new PriorityQueue<>();
+        for (int[] p : list) {
+            q.add(p[1]);
+            if (q.size() > p[0]) {
+                q.poll();
+            }
+        }
+
+        int sum = 0;
+        while (!q.isEmpty()) {
+            sum += q.poll();
+        }
+        System.out.println(sum);
+    }
+}
