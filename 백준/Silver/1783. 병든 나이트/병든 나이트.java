8e104277d4dccd4d9a97f270d4db223a4c01dce6
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-06-14 15:28:32 +0900

    [Silver III] Title: 병든 나이트, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        if (n == 1) count = 1;
+        else if (n == 2) count = Math.min(4, ((m + 1) / 2));
+        else if (m < 7) count = Math.min(4, m);
+        else count = m - 2;
+
+        System.out.println(count);
+    }
+}    
