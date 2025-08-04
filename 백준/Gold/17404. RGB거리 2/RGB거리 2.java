1f3ea4033d0637d3889726942730235e2369cedd
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-08-04 22:13:47 +0900

    [Gold IV] Title: RGB거리 2, Time: 88 ms, Memory: 12104 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] dp = new int[n][3];
+		int[][] house = new int[n][3];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			house[i][0] = Integer.parseInt(st.nextToken());
+			house[i][1] = Integer.parseInt(st.nextToken());
+			house[i][2] = Integer.parseInt(st.nextToken());
+		}
+		int ans = Integer.MAX_VALUE;
+
+		for (int i = 0; i < 3; i++) {
+			int result = Integer.MAX_VALUE;
+			for (int j = 0; j < 3; j++) {
+				if (j == i) {
+					dp[n - 1][j] = 1_000_001;
+					continue;
+				}
+				dp[n - 1][j] = house[n - 1][j];
+			}
+			for (int j = n - 2; j > 0; j--) {
+				dp[j][0] = Math.min(dp[j + 1][1], dp[j + 1][2]) + house[j][0];
+				dp[j][1] = Math.min(dp[j + 1][0], dp[j + 1][2]) + house[j][1];
+				dp[j][2] = Math.min(dp[j + 1][0], dp[j + 1][1]) + house[j][2];
+			}
+			for (int j = 0; j < 3; j++) {
+				if (i == j) {
+					continue;
+				}
+				result = Math.min(result, house[0][i] + dp[1][j]);
+			}
+			ans = Math.min(ans, result);
+		}
+
+		System.out.println(ans);
+	}
+}
