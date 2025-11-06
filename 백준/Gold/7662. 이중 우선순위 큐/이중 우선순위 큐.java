4a71ad4b1c45d297d4561e0981289d53173c6b59
ChanWon Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-11-06 22:03:48 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2484 ms, Memory: 439588 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,66 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        while (T-- > 0) {
-            int k = Integer.parseInt(br.readLine());
-            HashMap<Integer, Integer> map = new HashMap<>();
-            PriorityQueue<Integer> minPQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
+		for (int ti = 0; ti < t; ti++) {
+			int input = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < k; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String ch = st.nextToken();
-                int n = Integer.parseInt(st.nextToken());
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for (int i = 0; i < input; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String op = st.nextToken();
 
-                if ("I".equals(ch)) {
-                    map.put(n, map.getOrDefault(n, 0) + 1);
-                    minPQ.add(n);
-                    maxPQ.add(n);
-                } else {
-                    if (map.isEmpty()) continue;
-                    PriorityQueue<Integer> pq = n == 1 ? maxPQ : minPQ;
-                    removeMap(pq, map);
-                }
-            }
-            if (map.isEmpty()) {
-                sb.append("EMPTY").append("\n");
-            } else {
-                int n = removeMap(maxPQ, map);
-                sb.append(n).append(" ").append(!map.isEmpty() ? removeMap(minPQ, map) : n)
-                        .append("\n");
-            }
-        }
-        System.out.println(sb);
-    }
+				if (op.equals("I")) {
+					int num = Integer.parseInt(st.nextToken());
+					map.put(num, map.getOrDefault(num, 0) + 1);
+				} else {
+					if (map.size() == 0)
+						continue;
+					int type = Integer.parseInt(st.nextToken());
+					int num;
+					if (type == 1) {
+						num = map.lastKey();
+					} else {
+						num = map.firstKey();
+					}
+					if (map.put(num, map.get(num) - 1) == 1) {
+						map.remove(num);
+					}
+				}
+			}
 
-    static int removeMap(PriorityQueue<Integer> que, Map<Integer, Integer> map) {
-        int num;
-        while (true) {
-            num = que.poll();
-            int cnt = map.getOrDefault(num, 0);
+			if (map.size() == 0) {
+				sb.append("EMPTY\n");
+			} else {
+				sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
+			}
 
-            if (cnt == 0)
-                continue;
-
-            if (cnt == 1)
-                map.remove(num);
-            else
-                map.put(num, cnt - 1);
-
-            break;
-        }
-
-        return num;
-    }
+		}
+		System.out.println(sb);
+	}
 }
