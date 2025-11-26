71397fc07718f7466fd3656f8fe28c19bf76a5b5
ChanWon Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2025-11-26 21:10:35 +0900

    [Gold I] Title: 커피숍2, Time: 700 ms, Memory: 91448 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] arr;
+	static long[] tree;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+
+		arr = new int[n + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		tree = new long[n * 4];
+
+		init(1, n, 1);
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (x > y) {
+				int temp = y;
+				y = x;
+				x = temp;
+			}
+
+			sb.append(sum(1, n, 1, x, y)).append("\n");
+			update(1, n, 1, a, b);
+		}
+
+		System.out.println(sb);
+	}
+
+	static long init(int start, int end, int node) {
+		if (start == end) {
+			return tree[node] = arr[start];
+		}
+
+		int mid = (start + end) / 2;
+		return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+	}
+
+	static long sum(int start, int end, int node, int left, int right) {
+		if (end < left || right < start) {
+			return 0;
+		}
+
+		if (left <= start && end <= right) {
+			return tree[node];
+		}
+
+		int mid = (start + end) / 2;
+		return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+	}
+
+	static long update(int start, int end, int node, int idx, long val) {
+		if (idx < start || idx > end) {
+			return tree[node];
+		}
+
+		if (start == end) {
+			return tree[node] = val;
+		}
+
+		int mid = (start + end) / 2;
+		return tree[node] = update(start, mid, node * 2, idx, val) + update(mid + 1, end, node * 2 + 1, idx, val);
+	}
+
+}
